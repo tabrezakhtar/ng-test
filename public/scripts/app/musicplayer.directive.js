b342769fcb3e15d98b71a17442ef48ab12a899b2
tabrezakhtar <takhtar@brightsolid.com>
2015-06-12 02:06:07 +0100

    timer for progress

--- public/scripts/app/musicplayer.directive.js
+++ public/scripts/app/musicplayer.directive.js
@@ -1,22 +1,70 @@
-directiveModule.directive('musicPlayer', ['playlistService', function (playlistService) {
+directiveModule.directive('musicPlayer', ['$interval', 'playlistService', function ($interval, playlistService) {
 
     return {
         templateUrl: '/views/templates/musicplayer.tmpl.html',
         restrict: 'A',
-        // scope : {
-        //     ownerobject : '=ownerobject',
-        //     ownertypeid: '=ownertypeid',
-        //     itemsperpage: '=itemsperpage',
-        //     familytreetype: '=familytreetype'
-        // },
         link: function (scope, iElement, attr) {
-            scope.playerState = 'Play';
-            scope.songTime = 5;
-                
-            playlistService.getPlaylist().then(function(playlist) {
-                
-            });
+            var songInterval, 
+                currentSongIndex = 0,
+                playlist = null;
+
+            scope.model = {
+                playerState: 'Play',
+                songTime: 0,
+                currentSong: null
+            };            
             
+            scope.loadPlaylist = function(){
+                playlistService.getPlaylist().then(function(playlistFromServer) {
+                    if (playlistFromServer.length > 0) {
+                        playlist = playlistFromServer;
+                        startPlaying(playlist);                        
+                    }                 
+                });
+            };
+
+            scope.next = function(){
+                $interval.cancel(songInterval);
+                goNextSong();
+                startPlaying(playlist);
+            };
+
+            scope.prev = function(){
+                $interval.cancel(songInterval);
+                goPrevSong();
+                startPlaying(playlist);
+            };
+
+            function startPlaying(playlist){
+                scope.model.currentSong = playlist[currentSongIndex];
+                scope.model.playerState = 'Pause';
+                songInterval = $interval(
+                    function() {
+                        scope.model.songTime += 1;
+                        if (scope.model.songTime >= scope.model.currentSong.duration*60){
+                            goNextSong();
+                        }
+                    }, 1000
+                );   
+            }
+
+            function goNextSong(){
+                scope.model.songTime = 0;
+                currentSongIndex += 1;
+                if (currentSongIndex >= playlist.length){
+                    currentSongIndex = 0;
+                }
+                scope.model.currentSong = playlist[currentSongIndex];
+            }
+
+            function goPrevSong(){
+                scope.model.songTime = 0;
+                currentSongIndex -= 1;
+                if (currentSongIndex < 0){
+                    currentSongIndex = playlist.length-1;
+                }
+                scope.model.currentSong = playlist[currentSongIndex];
+            }
         }
     };
 }]);
